8ddf69668d33c5565df8482bd050203ba4327ff9
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-24 22:55:24 +0530

    modified

--- cart.sh
+++ cart.sh
@@ -33,11 +33,11 @@ else
      echo -e " $G you are using root user $N "
 fi 
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
+curl -sL -o https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
 
 VALIDATE $? "RPM Resouces downloaging"
 
-yum install nodejs -y   &>> $LOGFILE
+dnf install nodejs -y   &>> $LOGFILE
 
 VALIDATE $? "install nodejs"
 
