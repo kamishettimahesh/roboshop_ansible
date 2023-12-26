89cfe240350d4abeffd0c3ce9a94d2045c45db37
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-26 17:20:52 +0530

    ec2 -creation

--- ec2-creation.sh
+++ ec2-creation.sh
@@ -16,7 +16,6 @@ then
 else 
     
     INSTANCE_TYPE="t2.micro"
-
 fi
   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-030ba634926b78815 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
